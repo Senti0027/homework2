3f341952c3fb4ba6f8fc9ca61847f8d78db865c7
Senti0027 <5b0g0027@stust.edu.tw>
2023-09-25 21:24:44 +0800

    #01

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,46 +23,51 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
-        private bool count = false;
+        List<Triangle> triangles = new List<Triangle>();
+        double num, num2, num3;
         public MainWindow()
         {
             InitializeComponent();
         }
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            List<string> Triangle = new List<string>();
-            count = true;
-            double num,num2,num3;
             if ((double.TryParse(box.Text, out num)==false)|| (double.TryParse(box2.Text, out num2) == false) || (double.TryParse(box3.Text, out num3) == false))
             {
                 MessageBox.Show("請使用者重新輸入");
+                return;
             }
-            else if((num<0)|| (num2 < 0)|| (num3 < 0)) {
-                MessageBox.Show("數值小於0","請使用者重新輸入");
+            if((num<1)|| (num2 < 1)|| (num3 < 1)) {
+                MessageBox.Show("數值小於1","請使用者重新輸入");
+                return;
             }
-            else if((num+num2<num3) || (num2+num3 < num) || (num3+ num < num2)) 
-                {
-                lab.Background = Brushes.Red;
-                string n1 = $"邊長{num},{num2},{num3}不可構成三角形";
-                lab.Content = n1;
-                
-                string count= $"邊長{num},{num2},{num3}不可構成三角形";
+            if((num+num2>num3) && (num2+num3 > num) && (num3+ num > num2)) 
+            {
+                lab.Background = Brushes.DarkTurquoise;
+                lab.Content = $"邊長{num},{num2},{num3}可構成三角形";
+                triangles.Add(new Triangle() { Side1 = num, Side2 = num2, Side3 = num3, Istriangle = true });
             }
             else
             {
-                lab.Background = Brushes.Green;
-                string n1 = $"邊長{num},{num2},{num3}可構成三角形";
-                lab.Content = n1;
-
-                string count = $"邊長{num},{num2},{num3}可構成三角形";
+                lab.Background = Brushes.Red;
+                lab.Content = $"邊長{num},{num2},{num3}不可構成三角形";
+                triangles.Add(new Triangle() { Side1 = num, Side2 = num2, Side3 = num3, Istriangle = false });
             }
-
+            butter();
         }
-
-        private static void ListResult(List<string> myPrimes, string n)
+        private void butter()
         {
-
+            block.Text="";
+            foreach (Triangle t in triangles)
+            {
+                if (t.Istriangle)
+                {
+                    block.Text += $"邊長{t.Side1} {t.Side2} {t.Side3} 是三角形\n";
+                }
+                else
+                {
+                    block.Text += $"邊長{t.Side1} {t.Side2} {t.Side3} 不是三角形\n";
+                }
+            }
         }
-
     }
 }
